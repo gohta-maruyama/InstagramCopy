e7fd76564d26c20294acf6e6f7a141bde5291a21
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-11-24 10:11:34 +0900

    Initial Commit

---
+++ InstagramCopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramCopy
+//
+//  Created by 丸山昂大 on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
