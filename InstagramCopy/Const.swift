6a61dc996fecf3579cc1fbaf9115d8da4d592fb5
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-11-25 08:20:00 +0900

    構造体の作成方法がわからないです。

---
+++ InstagramCopy/Const.swift
@@ -0,0 +1,13 @@
+//
+//  Const.swift
+//  InstagramCopy
+//
+//  Created by 丸山昂大 on 2021/11/25.
+//
+
+import Foundation
+
+struct Const {
+    static let ImagePath = "images"
+    static let PostPathi = "posts"
+}
